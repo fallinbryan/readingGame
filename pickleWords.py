b634db16358a06cc287437f7c21e20f4ae63965c
fallinbryan <fallinbryan@ufl.edu>
2018-05-04 18:53:25 -0500

    add script to pickle words from text file

---
+++ pickleWords.py
@@ -0,0 +1,13 @@
+import pickle
+with open('core-wordnet.txt') as words_file:
+    raw_data = words_file.readlines()
+
+word_list = []
+for line in raw_data:
+    word_list.append(line.split()[2].replace(']','').replace('[',''))
+
+word_list = list(set(word_list))
+word_list = word_list.sort()
+
+with open('words.pickle','wb') as pf:
+    pickle.dump(word_list,pf)
