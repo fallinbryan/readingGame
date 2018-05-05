18307d918b523c67cfe751b6e311c6d79adbb972
fallinbryan <fallinbryan@ufl.edu>
2018-05-04 20:47:42 -0500

    fix bug in pickleWordss

--- pickleWords.py
+++ pickleWords.py
@@ -7,7 +7,7 @@ for line in raw_data:
     word_list.append(line.split()[2].replace(']','').replace('[',''))
 
 word_list = list(set(word_list))
-word_list = word_list.sort()
+word_list.sort()
 
 with open('words.pickle','wb') as pf:
     pickle.dump(word_list,pf)
