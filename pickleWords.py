bcba7a98b2f9fd1052cfe79e27a53d278f9baa62
fallinbryan <fallinbryan@ufl.edu>
2018-05-05 11:29:44 -0500

    remove non-kid friendly words from list

--- pickleWords.py
+++ pickleWords.py
@@ -2,9 +2,15 @@ import pickle
 with open('core-wordnet.txt') as words_file:
     raw_data = words_file.readlines()
 
+blockWords = ['sex','rape','faggot','shit','fuck','damn','pussy','penis','vagina','hell','cock',
+             'piss','nigger','penetrate','penetration','sexual','pervert','breast','kill','murder',
+             'boob','boobie','tit','titty','dick','ass','asshole','ass-hole']
+
 word_list = []
 for line in raw_data:
-    word_list.append(line.split()[2].replace(']','').replace('[',''))
+    word = line.split()[2].replace(']','').replace('[','')
+    if word not in blockWords:
+        word_list.append(word)
 
 word_list = list(set(word_list))
 word_list.sort()
